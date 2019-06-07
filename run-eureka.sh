c719c8db7bfbda8180f2998fd7c1786f9daf988c
CrisMon-01 <cristianmonti1995@gmail.com>
2019-06-07 20:39:48 +0200

    aggiunto .gitignore e modificato lo script eurekaserver

--- run-eureka.sh
+++ run-eureka.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 echo Running SENTENCE [Eureka + Discovery Client]
@@ -6,6 +5,4 @@ echo Running SENTENCE [Eureka + Discovery Client]
 echo Starting Eureka Server  
 java -Xms64m -Xmx128m -jar eureka-server/build/libs/eureka.jar &
 
-#echo Starting Sentence Service [*1, using Discovery Client]
-
-#java -Xms64m -Xmx128m -jar sentence-service/build/libs/sentence.jar &
+curl http://localhost:9090
