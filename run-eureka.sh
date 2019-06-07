b6ad962cf298cd305cd67b90026f822ededf426d
CrisMon-01 <cristianmonti1995@gmail.com>
2019-06-07 21:23:02 +0200

    modificato script

--- run-eureka.sh
+++ run-eureka.sh
@@ -5,6 +5,7 @@ echo Running SENTENCE [Eureka + Discovery Client]
 echo Starting Eureka Server  
 cd eureka-server
 gradle build
+cd ..
 java -Xms64m -Xmx128m -jar eureka-server/build/libs/eureka.jar &
 
 curl http://localhost:9090
