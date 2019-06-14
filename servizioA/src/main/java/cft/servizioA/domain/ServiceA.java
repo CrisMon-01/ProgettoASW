1cd2ebef0fc98b7814bae431957e4c9c052d2d67
fabiano <fabiano.dagostino96@gmail.com>
2019-06-14 20:37:16 +0200

    Modifica di diversi file di ServizioA

--- servizioA/src/main/java/cft/servizioA/domain/ServiceA.java
+++ servizioA/src/main/java/cft/servizioA/domain/ServiceA.java
@@ -1,10 +1,37 @@
 package cft.servizioA.domain;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceA {
-//TO-DO
+	
+	@Autowired
+	@Value("${spring.profiles}")
+	private String instanceName;
+	
+	//@Autowired
+	//@Value("${animals}")
+	//private String animals;
+	
+	//public String getAnimale() {
+	//	String[] animali = animals.split(",");
+	//	int i = (int) (Math.round(Math.random()*(animali.length-1)));
+	//	String animale = animali[i];	
+	//	return animale;
+	//}
+	
+	public String getName() {
+		return this.instanceName;
+	}
+	
+	public String getSentence() {
+		String sentence = this.getName();
+		//+": "+this.getAnimale();
+		return sentence;
+	}
 	
 }
