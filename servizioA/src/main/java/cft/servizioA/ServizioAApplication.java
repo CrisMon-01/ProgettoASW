2be4715ebdad1b344417a6808896ca00d4f1efbd
CrisMon-01 <cristianmonti1995@gmail.com>
2019-06-18 10:40:51 +0200

    dockerizzato servizio a & b

--- servizioA/src/main/java/cft/servizioA/ServizioAApplication.java
+++ servizioA/src/main/java/cft/servizioA/ServizioAApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient						//RIMUOVI PER TEST SENZA EUREKA
+@EnableDiscoveryClient						
 public class ServizioAApplication {
 
 	public static void main(String[] args) {
