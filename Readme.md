166eb51ea74c6cd8354728cda29049fd2b406f72
CrisMon-01 <cristianmonti1995@gmail.com>
2019-06-18 11:38:14 +0200

    aggiunto Readme provvisorio e script per lo start

---
+++ Readme.md
@@ -0,0 +1,7 @@
+##Progetto per ASW
+#Progetto 2D
+
+Per ora sono pronti solo il servizio A e B
+er far partire l'applicazione si può manualmente buildare i 2 servizi e eureka e poi dare il comando 'docker-compose up'
+Oppure
+usare lo script startup.sh con 'sh startup.sh'
---
+++ startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bashi
+
+echo !!! BUILD !!!
+echo BUILD EUREKA
+cd eureka-server
+gradle build
+cd ..
+echo BUILD SERVIZIO A
+cd servizioA
+gradle build
+cd ..
+echo BUILD SERVIZIO B
+cd servizioB
+gradle build
+cd ..
+echo BUILD SERVIZIO C
+cd servizioC
+gradle build
+#cd ..
+#echo !!! BUILD COMPLETE !!!
+#echo START APPLICAZIONE
+docker-compose up -d
