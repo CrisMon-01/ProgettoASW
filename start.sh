d11b3ee63778376417e32e876e7ec862c2839973
CrisMon-01 <cristianmonti1995@gmail.com>
2019-06-20 21:44:17 +0200

    creati script di avvio

---
+++ start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo SOURCE
+source set-docker-host-ip.sh
+
+echo START KAFKA IN DOCKER
+sh start-kafka.sh
+
+echo !!!BUILD!!!
+cd eureka-server
+gradle build
+cd ..
+cd servizioA
+gradle build
+cd ..
+cd servizioB
+gradle build
+cd ..
+cd servizioC
+gradle build 
+cd ..
+cd zuul-gateway
+gradle build 
+cd ..
+
+echo !!! ALL BUILD RUN APPLICATIONS !!!
+java -Xms64m -Xmx128m -jar eureka-serve/build/libs/eureka-server.jar &
+java -Xms64m -Xmx128m -jar servizioA/build/libs/servizioA.jar &
+java -Xms64m -Xmx128m -jar servizioB/build/libs/servizioB.jar &
+java -Xms64m -Xmx128m -jar servizioC/build/libs/servizioC.jar &
+java -Xms64m -Xmx128m -jar zuul-gateway/build/libs/zuul-gateway.jar
+
+echo !!! THE APPLICATION IS RUNNING !!!
