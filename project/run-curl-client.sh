188f197f3aaec66b36ee7132bb2c46c394a5e6c7
Fabiano <fabiano.dagostino96@gmail.com>
2019-06-29 16:37:17 +0200

    Modificato script per la verifica dell'applicazione.

--- project/run-curl-client.sh
+++ project/run-curl-client.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-# esegue POST e GET 
+FILE_NAME=$0
+if [ $# != 1 ]
+then
+    echo "Utilizzo: $FILE_NAME <numero_richieste>" 
+    echo "Se numero_richieste è pari a 0, il client effettua un numero infinito di richieste"
+    exit 1
+fi
 
-echo "Richiesta POST servizio A"
-curl -X POST localhost:8080/aservice
-echo "" 
-	
-echo "Richiesta GET servizio A"
-curl localhost:8080/aservice
-echo ""
+function richiesta {
+	echo "Richiesta POST servizio A"
+    curl -X POST localhost:8080/aservice
+    echo ""
+	echo "Richiesta GET servizio A"
+	curl localhost:8080/aservice
+	echo ""
+}
+
+REQUESTS=$1
+
+if [ $REQUESTS == 0 ]
+then
+    while true; do
+		sleep 3
+        richiesta
+    done
+else
+    for i in $(eval echo {1..$REQUESTS}); do
+		sleep 3
+		richiesta
+    done
+fi
